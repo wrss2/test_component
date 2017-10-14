6654e9bbd0382a36bbd8b5d2ef815b99d0ad44bb
wrss2 <wrsswrss@gmail.com>
2017-10-14 09:47:40 +0200

    zm

--- script.js
+++ script.js
@@ -1,46 +1,69 @@
-(function($){
-
-    var twitter_widget = function(element,options){
-        this.element = $(element);
-    };
+!(function($){
+  console.log("z1")
 
 
-    twitter_widget.prototype = {
-        constructor:twitter_widget,
-        init:function(text){
-            var html = '<div class="main">'+
-                            '<div class="header"></div>'+
-                            '<div class="content">' +
-                                '' +
-                                '' +
-                                '' +
-                                '</div>'+
-                        '</div>'
-          //  $(html).insertBefore(this.element);
-            $(this.element).remove();
-            $(".heart").click(function(){
-                console.log("heart");
-            });
-            $(".folow-button").click(function(){
-                console.log("folow-button");
-            });
-
-        }
-    };
+  $(".posts").append("<ul></ul>");
+  $.getJSON("data.json", function(json) {
+    $.each( json, function( key, val ) {
+      $(".posts ul").append(
+              '<li class="collection-item avatar">'+
+                  '<div class="wrapper-top">'+
+                        '<img src="'+val.avatar+'" alt="">'+
+                        '<span class="title_header">'+val.name+'</span>'+
+                        '<span class="day">'+val.day+'</span>'+
+                    '</div>'+
+                    '<p>'+val.message+'</p>'+
+                '</li>')
+    });
+    console.log(json);
+  });
+  console.log("z2")
 
 
+  $(".share").click(function(event){
+      console.log("share ");
+  });
+  $(".heart").click(function(event){
+      event.preventDefault();
+      var like = parseInt($(".likes:first-child h1").text())+1;
+      $(".likes:first-child h1").text(like);
+      console.log("heart "+like);
+  });
+  $(".folow-button").click(function(event){
+      event.preventDefault();
+      var follow = parseInt($(".likes:last-child h1").text())+1;
+      $(".likes:last-child h1").text(follow);
+      console.log("folow-button");
+  });
+  $(".comments").click(function(event){
+    event.stopPropagation();
 
-    $.fn.twitter = function(text){
-        var twit =   new twitter_widget(this,null);
-        twit.init(text);
 
-    };
+     $(".posts , .wrapper-comment").toggle();
+     if($('.posts').is(':visible')){
+         $(".content_follow").animate({height:'674px'});
+         $(".main").animate({height:'813px'});
+    }else{
+       $(".content_follow").animate({height:'60px'});
+      $(".main").animate({height:'310px'});
+    }
+  });
 
+  $(document).keypress(function(e) {
+    if(e.which == 13 && $(".input_cm").val() !="" ) {
+        console.log('You pressed enter!');
+        var coment = $(".input_cm").val();
+        $(".posts ul").append(
+          '<li class="collection-item avatar">'+
+            '<div class="wrapper-top">'+
+                  '<img src="images/avatar.jpg" alt="">'+
+                  '<span class="title_header">Mike Ross</span>'+
+                  '<span class="day">0d</span>'+
+              '</div>'+
+              '<p>'+coment+'</p>'+
+          '</li>'
 
-
-
-
-
-    $.fn.twitter.Constructor = twitter_widget;
-
+        );
+    }
+  });
 })(jQuery);
